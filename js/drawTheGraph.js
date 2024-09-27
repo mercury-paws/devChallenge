48504adb3b14882298e3e06e7ba1680dffc15bdc
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-09-27 17:24:59 +0200

    amend canvas

--- js/drawTheGraph.js
+++ js/drawTheGraph.js
@@ -1,8 +1,7 @@
 const drawDraph = document.querySelector(".drawDraph");
-
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 export const drawGraph = (graphType) => {
-  const canvas = document.getElementById("canvas");
-  const ctx = canvas.getContext("2d");
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   const xAxis = document.querySelector("input[name=xAxis]:checked");
